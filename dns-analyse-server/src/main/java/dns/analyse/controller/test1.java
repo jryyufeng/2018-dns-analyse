e86c7e08820315769b764dea0da41da81f20dbdf
jiangrongyin <jiangrongyin@meituan.com>
2019-02-21 11:09:10 +0800

    fastjson返回配置

--- dns-analyse-server/src/main/java/dns/analyse/controller/test1.java
+++ dns-analyse-server/src/main/java/dns/analyse/controller/test1.java
@@ -1,9 +1,34 @@
 package dns.analyse.controller;
 
+import dns.analyse.dao.model.DomainDependencePO;
+import dns.analyse.service.IDnsDomainDependenceService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Arrays;
+
+import java.util.List;
+
+
 /**
  * Author: jiangrongyin@meituan.com
  * Date: 2019/1/19 下午4:11
  * Description:
  */
+@Slf4j
+@Controller
+@RequestMapping("/TestDns")
 public class test1 {
+    @Autowired
+    IDnsDomainDependenceService dnsDomainDependenceService;
+    @ResponseBody
+    @GetMapping("/queryDependence")
+    public Object commonQuery() {
+        List<DomainDependencePO> list = dnsDomainDependenceService.queryByIds(Arrays.asList(1,2));
+        return list;
+    }
 }
